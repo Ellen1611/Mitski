1e9eab23f2b2db4cc7aace770e194307dfa36123
Ellen <120743592+Ellen1611@users.noreply.github.com>
2023-06-06 11:18:33 -0300

    Add files via upload

---
+++ side eye/agr.py
@@ -0,0 +1,16 @@
+#Programação com Python                   #
+#Aluno1: Ellen Giovanna                   #
+#Aluno2: Kauã Lucas                       #
+#Professor: Ramon                      
+#Turma: 2D                                #
+#Exercicio 04
+                  
+n1 = int(input("Digite um numero "))
+n2 = (n1%2)
+
+if (n2==1):
+    print("o numero é impar")
+elif (n2==0):
+     print("O numero é par")
+
+
---
+++ side eye/voltaMitski.py
@@ -0,0 +1,22 @@
+#Aula 01 
+#Data: 30/05
+#turma: 2 D
+#aluna:Ellen Giovanna
+
+#exemplo 1
+print ("cemicruzeiro.se.df.gov.br" .split (".") [0])
+print ('19:58:20' .split (':') [1])
+#exemplo 02
+y="cemicruzeiro.se.df.gov.br" .split (".")
+print (y)
+p='19:58:20' .split (':')
+print (p)
+#exemplo 03
+hora, minutos, segundos +'19:58:20' .split (':')
+print ("hora ->", hora,"\n", "Minutos ->", minuto, "\n", "Segundos ->", segundos, "\n" )
+#exemplo 04
+texto = "escola CEMIC 2022"
+print (len(texto))
+print (texto [10: ])
+print (texto [5:10])
+print (texto [-9])
